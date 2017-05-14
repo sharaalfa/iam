99087d21e614fd9fd04b00af851cb136d0664bdf
sharaalfa <stcon45@gmail.com>
2017-05-14 18:52:06 +0400

    add replace values

--- src/main/java/sharafutdinov/artur/iam/FromXMLToValue.java
+++ src/main/java/sharafutdinov/artur/iam/FromXMLToValue.java
@@ -1,5 +1,6 @@
 package sharafutdinov.artur.iam;
 
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,12 +10,29 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 
-/**
- * Created by first on 21.04.17.
- */
+
 public class FromXMLToValue {
-    private String value;
-   public String val(String path, String dir, String tag, int amount) {
+
+    /**
+     *  Логгирование
+     */
+   public static final Logger log = Logger.getLogger(FromXMLToValue.class);
+    /**
+     * значения номера найденного снилс
+     */
+   private String value;
+   private String value1;
+
+    /**
+     * Парсер файла XML
+     * @param path путь к файлу
+     * @param dir корневой элемент
+     * @param tag искомый тэг
+     * @param count порядковый номер снилс в XML
+     * @return
+     */
+
+   public String val(String path, String dir, String tag, int count) {
        try {
            File fromXML = new File(path);
            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -23,13 +41,16 @@ public class FromXMLToValue {
            document.getDocumentElement().normalize();
            NodeList nodeList = document.getElementsByTagName(dir);
 
-           Node node = nodeList.item(amount);
+           Node node = nodeList.item(count);
            Element element = (Element) node;
            value = element.getElementsByTagName(tag).item(0).getTextContent();
+           value1 = (value.replace("-", "")).replace(" ", "");
 
+       } catch (NullPointerException e) {
+           log.info("нет номера снилс");
        } catch (Exception e) {
-           e.printStackTrace();
+           log.error("проблема чтения xml");
        }
-       return value;
+       return value1; //value.substring(0, 3) + value.substring(4, 7) + value.substring(8, 13);
    }
 }
